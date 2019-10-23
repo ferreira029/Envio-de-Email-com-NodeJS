e65664d8f72f7d16725df04819514b29d19c12cf
Ferreira029 <marcsantosferreira@gmail.com>
2019-10-23 18:48:31 -0300

    ajustando package.json

---
+++ src/controllers/SendEmailMailTrapController.js
@@ -0,0 +1,7 @@
+const nodemailer = require('nodemailer');
+// TODO: Mudar controller para SendEmailController e fazer funções coom o nome do servidor web de email Ex. sendMailTrap() ou sendOutlook()
+module.exports = {
+    send(req, res) {
+        
+    }
+}
