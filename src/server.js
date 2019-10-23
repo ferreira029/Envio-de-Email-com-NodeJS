3b66db27ee60e042f06459dc6add39e66937382b
Ferreira029 <marcsantosferreira@gmail.com>
2019-10-23 13:42:17 -0300

    Ajustando Server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 const cors =  require('cors');
 const SendEmailMailTrapController = require('./controllers/SendEmailMailTrapController');
 
@@ -11,7 +12,47 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.post('/api/form/mail_trap', SendEmailMailTrapController.send);
+app.post('/api/form/mail_trap', (req, res) => {
+    nodemailer.createTestAccount((error, account) => {
+        const htmlEmail = `
+            <h3>Detalhes do contato - Site Msoftware</h3>
+            <ul>
+                <li>Name: ${req.body.name} </li>
+                <li>Email: ${req.body.email} </li>
+                <li>Phone: ${req.body.phone} </li>
+            </ul>
+            <h2>Mensagem do detalhe do site do cliente: </h2>
+            <p>${req.body.message}</p>
+        `;
+    
+        let transpoter = nodemailer.createTransport({
+            host: 'smtp.mailtrap.io',
+            port: 2525,
+            auth: {
+                user: '785500aaf9ad00',
+                pass: 'da4840519080d0',
+            }
+        });
+        let mailOptions = {
+            from: 'teste@teste.com',
+            to: 'msoftware.marc@outlook.com',
+            replyTo: 'test@testaccount.com',
+            subject: 'Nova mensagem',
+            text: req.body.message,
+            html: htmlEmail,
+        }
+    
+        transpoter.sendMail(mailOptions, (error, info) => {
+            if(error) {
+                return console.log(error);
+            }
+    
+            console.log('Message sent: %s', info.message);
+            console.log('Message URL: %s', nodemailer.getTestMessageUrl(info));
+    
+        });
+    });
+});
 
 const PORT = process.env.PORT || 3001;
 
